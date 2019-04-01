a50ddb566e405532c76d685557f91e8a9cfffeff
root <root@baxes.ru>
2019-04-01 19:21:00 +0300

    template

---
+++ www/local/templates/simple/header.php
@@ -0,0 +1,25 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+IncludeTemplateLangFile(__FILE__);
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?$APPLICATION->ShowHead();?>
+<script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<title><?$APPLICATION->ShowTitle()?></title>
+</head>
+<body>
+	
+<div id="panel"><?$APPLICATION->ShowPanel();?></div>
+<div class="container header">
+	<div class="row">
+        <div class="col-md-6 mx-auto centered">
+		 <h1>Test</h1>
+        </div>
+		
+	</div>
+</div>
